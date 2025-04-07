214866d084c182b76acfb5c8055255900ba67c9e
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-07 13:01:21 +0200

    algo filter created

--- arrayRevisited.ts
+++ arrayRevisited.ts
@@ -13,6 +13,28 @@ class ArrayRevisited {
     this.length++;
     return this.length;
   }
+
+  filter(callback: (value: string, index: number, array: string[]) => boolean): string[] {
+    const filteredArray: string[] = [];
+
+    for (let i = 0; i < this.value.length; i++) {
+      if (callback(this.value[i], i, this.value)) {
+        filteredArray.push(this.value[i]);
+      }
+    }
+
+    return filteredArray;
+  }
 }
 
 export default ArrayRevisited;
+
+const arrayRevisited = new ArrayRevisited();
+
+arrayRevisited.push('apple');
+arrayRevisited.push('banana');
+arrayRevisited.push('cherry');
+arrayRevisited.push('date');
+console.log({arrayRevisited});
+const filteredResult = arrayRevisited.filter(value => value.includes('a'));
+console.log('Tableau après filtrage avec "a":', filteredResult);
