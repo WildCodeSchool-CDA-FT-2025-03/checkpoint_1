81b1c88465e6513b993b9d12c85a80641e30008c
Yiroma <yiromaric@gmail.com>
2025-04-07 11:37:33 +0200

    [ADD] création de la BDD [UPDATE] correction du MPD

---
+++ BDD/job_board.sql
@@ -0,0 +1,51 @@
+DROP DATABASE IF EXISTS `job_board`;
+
+CREATE DATABASE IF NOT EXISTS `job_board` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE job_board;
+
+CREATE TABLE IF NOT EXISTS `candidate` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `login` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `phone_number` INT NOT NULL,
+    `desc` VARCHAR(1000) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS `recruiter` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `login` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS `company` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `desc` VARCHAR(1000) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS `job` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(255) NOT NULL,
+    `desc` VARCHAR(1000) NOT NULL,
+    `city` VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS `candidate_job` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `id_candidate` INT NOT NULL,
+    `id_job` INT NOT NULL,
+    FOREIGN KEY (`id_candidate`) REFERENCES `candidate`(`id`),
+    FOREIGN KEY (`id_job`) REFERENCES `job`(`id`),
+);
+
+CREATE TABLE IF NOT EXISTS `candidate_company` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `id_candidate` INT NOT NULL,
+    `id_company` INT NOT NULL,
+    FOREIGN KEY (`id_candidate`) REFERENCES `candidate`(`id`),
+    FOREIGN KEY (`id_company`) REFERENCES `company`(`id`),
+);
