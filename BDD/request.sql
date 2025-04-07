bbbcab27f72a35059cc46db1ab797b3ccc929194
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-07 12:42:09 +0200

    request of all candidates who have candidated at one company added

--- BDD/request.sql
+++ BDD/request.sql
@@ -9,3 +9,12 @@ SELECT Candidate.login, Candidate.firstName, Candidate.lastName, Candidate.email
 FROM Candidate
 JOIN Candidate_List ON Candidate.id = Candidate_List.id_candidate
 WHERE Candidate_List.id_jobOffer = 25;
+
+
+-- Les informations de tous les candidats qui ont postulé aux offres d'une entreprise
+SELECT Candidate.login, Candidate.firstName, Candidate.lastName, Candidate.email, Candidate.phone, Candidate.presentation_text
+FROM Candidate
+JOIN Candidate_List ON Candidate.id = Candidate_List.id_candidate
+JOIN JobOffer ON Candidate_List.id_jobOffer = JobOffer.id
+JOIN Company ON JobOffer.id_recruiter = Company.id_recruiter
+WHERE Company.id = 30;
