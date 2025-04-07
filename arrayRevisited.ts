8158ce99c1d23ed11b1c59f337edeea3dc961054
abdifatah <abdifatah4@msn.com>
2025-04-07 13:08:42 +0200

    array filter

--- arrayRevisited.ts
+++ arrayRevisited.ts
@@ -13,6 +13,26 @@ class ArrayRevisited {
     this.length++;
     return this.length;
   }
-}
 
+  filter(cb: (element: string, index: number, array: string[]) => boolean): string[] {
+    const result: string[] = [];
+    for (let i = 0; i < this.value.length; i++) {
+      if (cb(this.value[i], i, this.value)) {
+        result.push(this.value[i]);
+      }
+    }
+    console.log(result)
+    return result;
+  }
+}
+const myArray = new ArrayRevisited();
+const filter = (element: any) => element !== "three";
+myArray.push("one");
+myArray.push("two");
+myArray.push("three");
+myArray.push("four");
+myArray.push("five");
+myArray.push("six");
+
+myArray.filter(filter)
 export default ArrayRevisited;
