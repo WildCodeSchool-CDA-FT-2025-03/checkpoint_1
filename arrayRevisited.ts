cff32d4aa53578a8fa8d1fef25146ab396f9cdf6
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-07 10:08:33 +0200

    Algo ok, ajout de la methode filter + callback

--- arrayRevisited.ts
+++ arrayRevisited.ts
@@ -8,10 +8,25 @@ class ArrayRevisited {
     for (let i = 0; i < this.value.length; i++) {
       array[i] = this.value[i];
     }
+
     array[this.value.length] = el;
     this.value = array;
     this.length++;
     return this.length;
+  }
+
+  filter(callback: (el: string, index: number, array: string[]) => boolean): string[] {
+    const result: string[] = [];
+
+    for (let i = 0; i < this.value.length; i++) {
+      const element = this.value[i];
+    
+      if (callback(element, i, this.value)) {
+        result.push(element);
+      }
+    }
+
+    return result;    
   }
 }
 
