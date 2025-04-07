278796f07b9d4797f29397d1e26fcf11c57e0339
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-07 15:33:33 +0200

    correction join for inner join because it's depend of our SGBD

--- BDD/request.sql
+++ BDD/request.sql
@@ -7,14 +7,14 @@ SELECT * FROM JobOffer WHERE town LIKE '%Paris%';
 -- Les informations de tous les candidats qui ont postule a une offre precise
 SELECT Candidate.login, Candidate.firstName, Candidate.lastName, Candidate.email, Candidate.phone, Candidate.presentation_text
 FROM Candidate
-JOIN Candidate_List ON Candidate.id = Candidate_List.id_candidate
+INNER JOIN Candidate_List ON Candidate.id = Candidate_List.id_candidate
 WHERE Candidate_List.id_jobOffer = 25;
 
 
 -- Les informations de tous les candidats qui ont postulé aux offres d'une entreprise
 SELECT Candidate.login, Candidate.firstName, Candidate.lastName, Candidate.email, Candidate.phone, Candidate.presentation_text
 FROM Candidate
-JOIN Candidate_List ON Candidate.id = Candidate_List.id_candidate
-JOIN JobOffer ON Candidate_List.id_jobOffer = JobOffer.id
-JOIN Company ON JobOffer.id_recruiter = Company.id_recruiter
+INNER JOIN Candidate_List ON Candidate.id = Candidate_List.id_candidate
+INNER JOIN JobOffer ON Candidate_List.id_jobOffer = JobOffer.id
+INNER JOIN Company ON JobOffer.id_recruiter = Company.id_recruiter
 WHERE Company.id = 30;
