e944ac82db908860d0e0609402c62e6c01ba30a7
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-07 12:37:42 +0200

    request for candidates informations who have candidated at one job offer + request of job offer from Paris

--- BDD/request.sql
+++ BDD/request.sql
@@ -1,2 +1,11 @@
 -- Toutes les offres d'emploi
 SELECT * FROM JobOffer;
+
+-- Toutes les offres d'emploi a Paris
+SELECT * FROM JobOffer WHERE town LIKE '%Paris%';
+
+-- Les informations de tous les candidats qui ont postule a une offre precise
+SELECT Candidate.login, Candidate.firstName, Candidate.lastName, Candidate.email, Candidate.phone, Candidate.presentation_text
+FROM Candidate
+JOIN Candidate_List ON Candidate.id = Candidate_List.id_candidate
+WHERE Candidate_List.id_jobOffer = 25;
