9af72d654ba4469b5607f6f8b2ec4203f1bf8426
Yiroma <yiromaric@gmail.com>
2025-04-07 12:04:32 +0200

    [ADD] Ajout de données dans la table candidate_job

---
+++ BDD/data/candidate_job.sql
@@ -0,0 +1,10 @@
+insert into candidate_job (id, id_candidate, id_job) values (1, 5, 8);
+insert into candidate_job (id, id_candidate, id_job) values (2, 3, 12);
+insert into candidate_job (id, id_candidate, id_job) values (3, 9, 5);
+insert into candidate_job (id, id_candidate, id_job) values (4, 22, 9);
+insert into candidate_job (id, id_candidate, id_job) values (5, 53, 55);
+insert into candidate_job (id, id_candidate, id_job) values (6, 16, 43);
+insert into candidate_job (id, id_candidate, id_job) values (7, 32, 28);
+insert into candidate_job (id, id_candidate, id_job) values (8, 25, 15);
+insert into candidate_job (id, id_candidate, id_job) values (9, 47, 34);
+insert into candidate_job (id, id_candidate, id_job) values (10, 60, 10);
